db862e3fbd9e4746994e9ee86e8c180c4c104d5c
Cihan Eroğlu <ceroglu7@gmail.com>
2022-11-04 00:01:40 +0300

    Basket module edited

--- a101-finalcase/src/test/java/Basket.java
+++ a101-finalcase/src/test/java/Basket.java
@@ -11,7 +11,8 @@ import java.util.Set;
 public class Basket {
     Actions actions;
     WebDriver driver;
-    String myProductInBasketXpath="/html/body/div/div/div/div[2]/div/div[2]/section/section/div[3]/div/ul/li/div/div/div[1]/div[2]/div[2]/a";
+    String myFirstProductInBasketXpath="/html/body/div/div/div/div[2]/div/div[2]/section/section/div[3]/div/ul/li/div/div/div[1]/div[2]/div[2]/a";
+    String mySecondProductInBasketXpath="/html/body/div[1]/div/div/div[2]/div/div[2]/section/section/div[4]/div/ul/li/div/div/div[1]/div[2]/div[2]/a";
     public Basket() {
         driver = Utils.getDriver();
         actions = new Actions(driver);
